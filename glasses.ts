15309239ecb2d65595a9dd4528cdf14e8dfb98c8
Tom MacWright <tom@macwright.com>
2023-11-16 12:06:01 -0500

    Remove conversion

--- glasses.ts
+++ glasses.ts
@@ -1,75 +1,54 @@
-import { camelCase } from "https://deno.land/x/case/mod.ts";
+import { Glass } from "./types.ts";
 
-export const glasses = [
-  {
-    name: "Collins glass",
-    icon: "M13 4H7v12.5c.5.25 1.5.5 3 .5s2.712-.24 3-.5V4zm-6 6.554l6-.054",
-  },
-  {
-    name: "Highball glass",
-    icon: "M13 4H6v11c.512.239 1.792.5 3.5.5s3.158-.261 3.5-.5V4zm-7 6h7",
-  },
-  {
-    name: "Old fashioned glass",
-    icon: "M14 5H6v10c.582.209 2.06.5 4 .5s3.612-.291 4-.5V5zm-8 5h8",
-  },
-  {
-    name: "Champagne coupe",
-    icon:
-      "M9.25 16.5v-8L8 7 6 6 4.5 4.5V3h11v1.5l-1.5 2-2 .5-1.25 1.5v8h1.582v.5H7.196v-.5H9.25z",
-  },
-  {
-    name: "Champagne flute",
-    icon:
-      "M9.25 17v-5.616L8 7.5 8.5 3h3l.5 4.5-1.25 3.884V17H12v.63H8V17h1.25zM8 4.856h3.616",
-  },
-  {
-    name: "Cocktail glass",
-    icon:
-      "M9 9.5L5.5 4h9l-3.501 5.5v4.75H13v1.25H7v-1.25h2.056L9 9.5zm-2.5-4h7",
-  },
-  {
-    name: "Hurricane",
-    icon:
-      "M9.25 17.5V14L7.5 12.5 7 10l.5-1.75.25-1.75-.25-1.75L7 3h6l-.5 1.75-.25 1.75.25 1.75L13 10l-.5 2.5-1.75 1.5v3.5h2.05v.5H7.196v-.5H9.25z",
-  },
-  {
-    name: "Margarita glass",
-    icon:
-      "M9.25 16.5V9L7.8 8l-.3-1.5L5 5.25 4.5 3h11L15 5.25 12.5 6.5 12.2 8l-1.45 1v7.5h1.582v.5H7.196v-.5H9.25z",
-  },
-  {
-    name: "Wine glass",
-    icon:
-      "M9.25 16.5v-6L7.8 9 7 7l1-4h4l1 4-.8 2-1.45 1.5v6h1.582v.5H7.196v-.5H9.25z",
-  },
-  {
-    name: "Copper mug",
-    icon:
-      "M13 5H5v9.973c.593.213 2.024.527 4 .527s3.605-.314 4-.527V5zm0 2.5h3V12h-3",
-  },
-  {
-    name: "Irish coffee mug",
-    icon:
-      "M9 15v-2l-1-1-1-.5V4h6v7.5l-1 .5-1 1v2l1.332.5v.5H7.196v-.5L9 15zm4-9.5h2.5V9L13 10M7 6.5h6",
-  },
-  {
-    name: "Steel cup",
-    icon:
-      "M14.5 4h-9L7 14.5c.5.25.75.5 3 .5 1.75 0 2.75-.25 3-.5L14.5 4zM6 6h8",
-  },
-  {
-    name: "Zombie glass",
-    icon: "M13 4H6v11c.512.239 1.792.5 3.5.5s3.158-.261 3.5-.5V4zm-7 6h7",
-  },
-];
-
-console.log(`import { Glass } from "./types.ts"`);
-
-for (const glass of glasses) {
-  console.log(
-    `export const ${camelCase(glass.name)} = new Glass(${
-      JSON.stringify(glass.name)
-    }, ${JSON.stringify(glass.icon)});`,
-  );
-}
+export const collinsGlass = new Glass(
+  "Collins glass",
+  "M13 4H7v12.5c.5.25 1.5.5 3 .5s2.712-.24 3-.5V4zm-6 6.554l6-.054",
+);
+export const highballGlass = new Glass(
+  "Highball glass",
+  "M13 4H6v11c.512.239 1.792.5 3.5.5s3.158-.261 3.5-.5V4zm-7 6h7",
+);
+export const oldFashionedGlass = new Glass(
+  "Old fashioned glass",
+  "M14 5H6v10c.582.209 2.06.5 4 .5s3.612-.291 4-.5V5zm-8 5h8",
+);
+export const champagneCoupe = new Glass(
+  "Champagne coupe",
+  "M9.25 16.5v-8L8 7 6 6 4.5 4.5V3h11v1.5l-1.5 2-2 .5-1.25 1.5v8h1.582v.5H7.196v-.5H9.25z",
+);
+export const champagneFlute = new Glass(
+  "Champagne flute",
+  "M9.25 17v-5.616L8 7.5 8.5 3h3l.5 4.5-1.25 3.884V17H12v.63H8V17h1.25zM8 4.856h3.616",
+);
+export const cocktailGlass = new Glass(
+  "Cocktail glass",
+  "M9 9.5L5.5 4h9l-3.501 5.5v4.75H13v1.25H7v-1.25h2.056L9 9.5zm-2.5-4h7",
+);
+export const hurricane = new Glass(
+  "Hurricane",
+  "M9.25 17.5V14L7.5 12.5 7 10l.5-1.75.25-1.75-.25-1.75L7 3h6l-.5 1.75-.25 1.75.25 1.75L13 10l-.5 2.5-1.75 1.5v3.5h2.05v.5H7.196v-.5H9.25z",
+);
+export const margaritaGlass = new Glass(
+  "Margarita glass",
+  "M9.25 16.5V9L7.8 8l-.3-1.5L5 5.25 4.5 3h11L15 5.25 12.5 6.5 12.2 8l-1.45 1v7.5h1.582v.5H7.196v-.5H9.25z",
+);
+export const wineGlass = new Glass(
+  "Wine glass",
+  "M9.25 16.5v-6L7.8 9 7 7l1-4h4l1 4-.8 2-1.45 1.5v6h1.582v.5H7.196v-.5H9.25z",
+);
+export const copperMug = new Glass(
+  "Copper mug",
+  "M13 5H5v9.973c.593.213 2.024.527 4 .527s3.605-.314 4-.527V5zm0 2.5h3V12h-3",
+);
+export const irishCoffeeMug = new Glass(
+  "Irish coffee mug",
+  "M9 15v-2l-1-1-1-.5V4h6v7.5l-1 .5-1 1v2l1.332.5v.5H7.196v-.5L9 15zm4-9.5h2.5V9L13 10M7 6.5h6",
+);
+export const steelCup = new Glass(
+  "Steel cup",
+  "M14.5 4h-9L7 14.5c.5.25.75.5 3 .5 1.75 0 2.75-.25 3-.5L14.5 4zM6 6h8",
+);
+export const zombieGlass = new Glass(
+  "Zombie glass",
+  "M13 4H6v11c.512.239 1.792.5 3.5.5s3.158-.261 3.5-.5V4zm-7 6h7",
+);
