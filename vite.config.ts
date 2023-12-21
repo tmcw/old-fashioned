b50f33cc61f758c98a5f279e7fcc97e72aff2d7b
Tom MacWright <tom@macwright.com>
2023-12-21 17:02:14 -0500

    Vite

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import pages from "@hono/vite-cloudflare-pages";
+import devServer from "@hono/vite-dev-server";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [
+    pages(),
+    devServer({
+      entry: "src/index.tsx",
+    }),
+  ],
+});
